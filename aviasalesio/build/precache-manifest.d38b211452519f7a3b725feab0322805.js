d253359c7f83cbc4fe4fd62a3645d7521905d269
maestro <adan3891@gmail.com>
2019-06-13 17:55:24 +0300

    start app

---
+++ aviasalesio/build/precache-manifest.d38b211452519f7a3b725feab0322805.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17776810aa02bf827e6ee205fcbbbc96",
+    "url": "/aviasalesio/index.html"
+  },
+  {
+    "revision": "cc9c8d3f03860b0c8878",
+    "url": "/aviasalesio/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/aviasalesio/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "cc9c8d3f03860b0c8878",
+    "url": "/aviasalesio/static/js/main.b6de09de.chunk.js"
+  },
+  {
+    "revision": "83bd9e4ee4cef0d2ed59",
+    "url": "/aviasalesio/static/js/runtime~main.a5b7213b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/aviasalesio/static/media/logo.5d5d9eef.svg"
+  }
+]);
